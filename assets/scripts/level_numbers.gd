ae23a436e59b8f7b3cf76be49474f940a61f179f
Anthony Blount <anthony00.blount00@gmail.com>
2026-07-25 16:33:21 -0500

    stylebox improvements and level numbers

--- assets/scripts/level_numbers.gd
+++ assets/scripts/level_numbers.gd
@@ -4,6 +4,6 @@ func _ready():
 	var file_name : String = get_parent().get_parent().get_scene_file_path()  # e.g., "level_1.tscn"
 	file_name = file_name.split("/")[-1]
 	file_name = file_name.trim_suffix(".tscn")  # Remove the ".tscn" extension
-	file_name = file_name.replace("_", " ")  # Replace underscores with spaces
+	file_name = file_name.replace("level_", " ")  # Replace underscores with spaces
 	file_name = file_name.capitalize()  # Convert to proper case
 	text = file_name
