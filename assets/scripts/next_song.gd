e62988c3e9cb518d1a93302477df03468fa5ccca
Anthony Blount <anthony00.blount00@gmail.com>
2026-07-25 12:40:54 -0500

    adjustable music volume credits title text

--- assets/scripts/next_song.gd
+++ assets/scripts/next_song.gd
@@ -1,9 +1,16 @@
 extends AudioStreamPlayer
 
 @export var next_song: AudioStreamPlayer
+var starting_volume : float
 
 func _ready():
     finished.connect(play_next_song)
+    starting_volume = volume_linear
+
+func _process(delta: float):
+    var music_volume : float = PlayerConfig.get_config(AppSettings.AUDIO_SECTION, "Music", 1.0)
+    print(music_volume)
+    volume_linear = starting_volume * music_volume
 
 func play_next_song():
     next_song.play()
