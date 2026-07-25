79ad8f2b90d1ac250a667af4f6b208eb6fca97e1
Anthony Blount <anthony00.blount00@gmail.com>
2026-07-25 12:56:15 -0500

    functioning sound effect volume controls

---
+++ assets/scripts/sound_effect_volume.gd
@@ -0,0 +1,11 @@
+extends AudioStreamPlayer2D
+
+var starting_volume : float
+
+func _ready():
+    starting_volume = volume_linear
+
+func _process(delta: float):
+    var sfx_volume : float = PlayerConfig.get_config(AppSettings.AUDIO_SECTION, "Sfx", 1.0)
+    print(sfx_volume)
+    volume_linear = starting_volume * sfx_volume
